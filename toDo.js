2533d5f9de5654bfc520068e7be227961df8a4dc
Maria Garcia <licgarcia_psic@hotmail.com>
2023-09-29 23:47:07 -0600

    complete

--- toDo.js
+++ toDo.js
@@ -1,20 +1,10 @@
-//lugar donde guardar los valores, las distintas cosas de la lista (varios valores >> array )
-//aca recupero localStorage.getItem 
-
 let aJson = JSON.parse(localStorage.getItem('tareas'));
-//Verifica que Json sea un arreglo, en caso de ser null da un arreglo vacío
-let datos = Array.isArray(aJson) ? aJson : [];
-
-let id = 1;
+// Si no hay datos válidos en el LocalStorage, utiliza un arreglo vacío
+let datos = Array.isArray(aJson) ? aJson : []; 
 
 const input = document.querySelector('#inputAgregar');
-
 const boton = document.querySelector('.agregar');
-
 const lista = document.querySelector('.lista');
-
-// const botonELiminar = document.querySelector('.eliminar');
-
 boton.addEventListener('click', agregarTareas);
 
 function agregarTareas(){
@@ -23,6 +13,8 @@ function agregarTareas(){
         console.log("esta vacío")
         return;
     }
+    //obteniendo el siguiente valor
+    let id = datos.length > 0 ? datos[datos.length - 1].llave + 1 : 1; 
     datos.push({llave:id, tarea:agregado});
     console.log(datos);
     input.value = '';
@@ -48,21 +40,11 @@ function mostrarLista(){
 
 const eliminar = (e) => {    
     console.log(e.target.value);
-    /*
-    datos.forEach(element =>{
-        if(parseInt(e.target.value) === element.llave){
-            console.log("Borro: " + element.tarea);
-            datos.splice(element.llave-1, 1);
-        }
-    })
-*/
     let temp = parseInt(e.target.value);
     const nuevoArreglo = datos.filter(elemento => 
         temp !== elemento.llave
     )
-    
-
-    console.log(nuevoArreglo);  
+        console.log(nuevoArreglo);  
     datos = nuevoArreglo;
     mostrarLista()
     localStorage.setItem('tareas', JSON.stringify(datos));
